d2b081958b4027e419920afe15ebe726cfbfe79d
Christophe De Troyer <christophe.detroyer@gmail.com>
2025-06-09 23:34:59 +0200

    native works

--- lib/secure_json.ex
+++ lib/secure_json.ex
@@ -1,10 +1,9 @@
 defmodule LocalDomain.SecureJson do
   use Rustler,
     otp_app: :local_domain,
-    crate: :host
+    crate: :secure_json
 
   # When loading a NIF module, dummy clauses for all NIF function are required.
   # NIF dummies usually just error out when called when the NIF is not loaded, as that should never normally happen.
-  def main(), do: :erlang.nif_error(:nif_not_loaded)
-
+  def add(_arg1, _arg2), do: :erlang.nif_error(:nif_not_loaded)
 end

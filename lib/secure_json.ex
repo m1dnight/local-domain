c45729e7048f3d927f95503527eb499831178e54
Christophe De Troyer <christophe.detroyer@gmail.com>
2025-06-10 12:01:34 +0200

    wip commit

--- lib/secure_json.ex
+++
@@ -1,9 +0,0 @@
-defmodule LocalDomain.Host do
-  use Rustler,
-    otp_app: :local_domain,
-    crate: :host
-
-  # When loading a NIF module, dummy clauses for all NIF function are required.
-  # NIF dummies usually just error out when called when the NIF is not loaded, as that should never normally happen.
-  def test(), do: :erlang.nif_error(:nif_not_loaded)
-end
---
+++ lib/zkvm.ex
@@ -0,0 +1,9 @@
+defmodule Zkvm do
+  use Rustler,
+    otp_app: :local_domain,
+    crate: :zkvm
+
+  # When loading a NIF module, dummy clauses for all NIF function are required.
+  # NIF dummies usually just error out when called when the NIF is not loaded, as that should never normally happen.
+  def add(_x, _y), do: :erlang.nif_error(:nif_not_loaded)
+end

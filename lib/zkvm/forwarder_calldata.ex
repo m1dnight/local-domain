5bfed4723425176619498449c824b1c5fa1bae88
Christophe De Troyer <christophe.detroyer@gmail.com>
2025-06-17 16:42:13 +0200

    wip commit

--- lib/zkvm/forwarder_calldata.ex
+++ lib/zkvm/forwarder_calldata.ex
@@ -2,8 +2,9 @@ defmodule Anoma.Zkvm.ForwarderCalldata do
   use TypedStruct
 
   typedstruct do
-    field :untrusted_forwarder, binary()
-    field :input, binary()
-    field :output, binary()
+    # field :untrusted_forwarder, binary()
+    # field :input, binary()
+    # field :output, binary()
+    field :name, number()
   end
 end
--- native/zkvm/src/lib.rs
+++ native/zkvm/src/lib.rs
@@ -6,21 +6,63 @@ use risc0_zkvm::Receipt;
 use rustler;
 
 use aarm::action::ForwarderCalldata;
-use rustler::nif;
+use aarm_core::resource::Resource;
+use rustler::{nif, Atom, Decoder, Encoder, Env, Error, NifResult, NifStruct, Term};
+
+use std::ops::Deref;
 
 mod prover;
+
+#[derive(Debug)]
+struct ResourceP {
+    pub name: u64,
+}
+
+impl<'a> Decoder<'a> for ResourceP {
+    fn decode(term: Term<'a>) -> NifResult<Self> {
+        // For decoding from a map/keyword list
+        let map = term.decode::<std::collections::HashMap<String, Term>>()?;
+
+        let name = map.get("name").ok_or(Error::BadArg)?.decode::<u64>()?;
+        //
+        // let age = map.get("age")
+        //     .ok_or(Error::BadArg)?
+        //     .decode::<i32>()?;
+        //
+        // let active = map.get("active")
+        //     .ok_or(Error::BadArg)?
+        //     .decode::<bool>()?;
+
+        Ok(ResourceP { name: name })
+    }
+}
+
+impl Encoder for ResourceP {
+    fn encode<'a>(&self, env: Env<'a>) -> Term<'a> {
+        use rustler::types::map::map_new;
+
+        let map = map_new(env);
+        let map = map
+            .map_put(Atom::from_str(env, "name").unwrap(), self.name.encode(env))
+            .unwrap();
+        let map = map
+            .map_put(
+                Atom::from_str(env, "__struct__").unwrap(),
+                Atom::from_str(env, "Elixir.Anoma.Zkvm.ForwarderCalldata").unwrap(),
+            )
+            .unwrap();
+
+        map
+    }
+}
 
 //----------------------------------------------------------------------------//
 //                                Functions                                   //
 //----------------------------------------------------------------------------//
 
 #[nif]
-fn testfunc() -> ForwarderCalldata {
-    ForwarderCalldata {
-        untrusted_forwarder: vec![],
-        input: vec![],
-        output: vec![],
-    }
+fn testfunc() -> ResourceP {
+    ResourceP { name: 123 }
 }
 
 #[nif]
